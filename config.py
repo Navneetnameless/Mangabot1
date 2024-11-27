8e8e9481720686e054a81fef9c2488c13b010e95
Aaru <aryarai1569@protonmail.com>
2024-11-27 13:13:02 +0530

    config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ env_vars = {
   # Get From my.telegram.org
   "API_ID": "20445873",
   #Get For @BotFather
-  "BOT_TOKEN": "7933592415:AAHfm7Om-nIZHhC-44XqdjHaS3Po2Wje-Gw",
+  "BOT_TOKEN": "7716045686:AAEF1du2uLWudLXUH-z-uVpTF5QqmEjIINA",
   # Get For tembo.io
   "DATABASE_URL_PRIMARY": "postgresql://postgres:4NQDGksWwMhyfcP5@delicately-purposeful-hen.data-1.apse1.tembo.io:5432/postgres",
   # Logs Channel Username Without @
@@ -18,7 +18,7 @@ env_vars = {
   # Ex : Chapter {chap_num} {chap_name} @Manhwa_Arena
   "FNAME": "[MC] [{chap_num}] {chap_name} @Manga_Campus"
 }
-OWNER_ID = int(os.environ.get("OWNER_ID", "7607741983")) # Retrieve the AUTH_USERS environment variable as a space-separated string and convert to a list of integers 
+OWNER_ID = int(os.environ.get("OWNER_ID", "1788144071")) # Retrieve the AUTH_USERS environment variable as a space-separated string and convert to a list of integers 
 auth_users = [int(user_id) for user_id in os.environ.get('5164955785').split()] # Append OWNER_ID to the list of auth_users 
 AUTH_USERS = auth_users + [OWNER_ID]
 dbname = env_vars.get('DATABASE_URL_PRIMARY') or env_vars.get('DATABASE_URL') or 'sqlite:///test.db'
