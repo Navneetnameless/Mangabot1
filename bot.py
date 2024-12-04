dfea78f29b975103e201a2bef51638b1ea37eff7
justatest420 <justatestsubject.01@gmail.com>
2024-12-04 15:57:14 +0545

    Update bot.py

--- bot.py
+++ bot.py
@@ -211,24 +211,20 @@ async def on_help(client: Client, message: Message):
 async def on_set_caption(client: Client, message: Message):
 	db = DB()
 	user_info = db.get_user(str(message.from_user.id))
-	text = f"""<b><i>
-For Manga Camps:
-Thumb : <code>{env_vars["TH1"]}</code>
-Banner: <code>{env_vars["B1"]}</code>
-
-For Weebs:
-Thumb: <code>{env_vars["TH2"]}</code>
-Bannar: <code>{env_vars["B2"]}</code>
-
-To Views Photo:
-   <code>/pto photoname.jpg</code>
-   
-Your Currnet Settings:
-Caption: <code>{user_info.caption if user_info.caption else "None"}</code>
-Thumb: <code>{user_info.thumb if user_info.thumb else "None"}</code>
-Banner: <code>{user_info.banner if user_info.banner else " None"}</code>
-
-To Change Your Settings 👇👇</i></b>"""
+	text = f"""
+ <b><i>For Manga Camps:
+ Thumb : <code>{env_vars["TH1"]}</code>
+ Banner: <code>{env_vars["B1"]}</code>
+ For Weebs:
+ Thumb: <code>{env_vars["TH2"]}</code>
+ Bannar: <code>{env_vars["B2"]}</code>
+ To Views Photo: <code>/pto photoname.jpg</code>
+ 
+ Your Currnet Settings:
+ Caption: <code>{user_info.caption if user_info.caption else "None"}</code>
+ Thumb: <code>{user_info.thumb if user_info.thumb else "None"}</code>
+ Banner: <code>{user_info.banner if user_info.banner else " None"}</code>
+ To Change Your Settings 👇👇</i></b>"""
 	return message.reply(text, reply_markup=sb)
 	#cap = UserInfo(user_id=str(message.from_user.id), caption=caption)
         #await db.add(user_options)
